1154cc906969a3a527bdda63b4ec19bdf67d8a81
Sergio Maeso <sergio@cloudflare.com>
2022-08-08 18:42:14 +0100

    Added first function

---
+++ functions/json.ts
@@ -0,0 +1,27 @@
+export async function onRequest(context) {
+    // Contents of context object
+    const {
+      request, // same as existing Worker API
+      env, // same as existing Worker API
+      params, // if filename includes [id] or [[path]]
+      waitUntil, // same as ctx.waitUntil in existing Worker API
+      next, // used for middleware or to fetch assets
+      data, // arbitrary space for passing data between middlewares
+    } = context;
+  
+    let obj =   {
+        "src": request.headers.get('CF-Connecting-IP'),
+        "host": request.headers.get('Host'),
+        "method": request.method,
+        "protocol": request.cf.httpProtocol,
+        "path": request.url,
+        "size": 0,
+        "tls": {
+          "version": request.cf.tlsVersion,
+          "cipher": request.cf.tlsCipher,
+          "mtls": request.cf.tlsClientAuth
+        },
+        "headers": request.headers
+      }
+    return new Response(JSON.stringify(obj, null, 2));
+  }
